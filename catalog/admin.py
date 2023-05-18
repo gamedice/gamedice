801b5eda3ded316d5a90a807159c63e06ecfa032
Ali023-Re <rekaloalina@gmail.com>
2023-05-18 17:00:17 +0300

    app catalog

---
+++ catalog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import Genre, Company, Games
+
+
+admin.site.register(Genre)
+admin.site.register(Company)
+admin.site.register(Games)
---
+++ catalog/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render
+from django_filters import FilterSet, NumberFilter, AllValuesFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, viewsets
+from rest_framework.pagination import PageNumberPagination
+from .models import Genre, Company, Games
+from .serializers import GenreSerializer, CompanySerializer, GamesSerializer, AnonsSerializer
+from rest_framework import filters
+from django.db.models import Max
+from random import randint
+
+
+class GamesFilter(FilterSet):
+    min_rating = NumberFilter(field_name='rating', lookup_expr='gte')
+    max_rating = NumberFilter(field_name='rating', lookup_expr='lte')
+    genre_name = AllValuesFilter(field_name='genre__name')
+    company_name = AllValuesFilter(field_name='company__name')
+
+    class Meta:
+        model = Games
+        fields = ('name', 'min_rating', 'max_rating', 'genre_name', 'company_name')
+
+
+class GameAPIListPagination(PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class GenreAPIView(viewsets.ReadOnlyModelViewSet):
+    queryset = Genre.objects.all().order_by('name')
+    serializer_class = GenreSerializer
+    pagination_class = GameAPIListPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
+
+
+class CompanyAPIView(viewsets.ReadOnlyModelViewSet):
+    queryset = Company.objects.all().order_by('name')
+    serializer_class = CompanySerializer
+    pagination_class = GameAPIListPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
+
+
+class GamesAPIView(viewsets.ReadOnlyModelViewSet):
+    queryset = Games.objects.all().order_by('name').filter(preview=True)
+    serializer_class = GamesSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_class = GamesFilter
+    # filterset_fields = ['genre', 'company']
+    # search_fields = ['genre__name', 'name']
+    pagination_class = GameAPIListPagination
+
+
+class TopAPIView(viewsets.ReadOnlyModelViewSet):
+    queryset = Games.objects.all().filter(preview=True).order_by('-rating')
+    serializer_class = GamesSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['rating']
+    pagination_class = GameAPIListPagination
+
+
+class AnonsAPIView(viewsets.ReadOnlyModelViewSet):
+    queryset = Games.objects.all().filter(preview=False).order_by('name')
+    serializer_class = AnonsSerializer
+    pagination_class = GameAPIListPagination
+
+
+class RandomAPIView(viewsets.ReadOnlyModelViewSet):
+    queryset = Games.objects.all()
+    serializer_class = GamesSerializer
+
+    def get_queryset(self):
+        max_id = Games.objects.all().aggregate(max_id=Max("id"))['max_id']
+        while True:
+            pk = randint(1, max_id)
+            category = Games.objects.filter(pk=pk)
+            if category:
+                return category
