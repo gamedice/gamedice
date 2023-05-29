7eeefa5eaf3c15c1346461819ea5c073a0085e0b
Boguk <boguk224@gmail.com>
2023-05-29 12:47:08 +0300

    fix images

--- conf/urls.py
+++ conf/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,7 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('profile/', include('login.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
