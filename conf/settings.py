56a4fae86e6d2abb9b9ee697c36969ec5c568e0a
Boguk <boguk224@gmail.com>
2023-05-29 11:09:16 +0300

    fix images

--- conf/settings.py
+++ conf/settings.py
@@ -143,8 +143,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_URL = '/assets/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
